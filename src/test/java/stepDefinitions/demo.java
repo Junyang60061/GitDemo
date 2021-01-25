be0429f869037cd699ab9423673f7d802c6de575
junyang60061 <junyang_60061@yahoo.com>
2021-01-25 17:56:09 -0600

    final commit by US add new object

---
+++ src/test/java/stepDefinitions/demo.java
@@ -0,0 +1,8 @@
+package stepDefinitions;
+
+public class demo {
+	private void Print() {
+		// TODO Auto-generated method stub
+		System.out.println("This is a test for code git branch merge");
+	}
+}
