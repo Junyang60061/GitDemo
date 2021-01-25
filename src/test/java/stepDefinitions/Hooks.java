59b74a55a49a43caefee49cd1b7ce13cc5743e78
junyang60061 <junyang_60061@yahoo.com>
2021-01-25 13:48:22 -0600

    JY first  commit 2021

---
+++ src/test/java/stepDefinitions/Hooks.java
@@ -0,0 +1,23 @@
+package stepDefinitions;
+
+import java.io.IOException;
+
+import io.cucumber.java.Before;
+
+public class Hooks {
+	
+	@Before("@DeletePlace")
+	public void beforeScenario() throws IOException
+	{
+		//write the code that iwll give you place id
+		//execute this code only when place id is null
+		
+		stepDefinitions m= new stepDefinitions();
+		if (stepDefinitions.place_id==null)
+		{	
+		m.add_place_payload_with("Mike Rezek", "English", "Nowhere");
+		m.user_calls_with_http_request("AddPlaceAPI", "POST");
+		m.verify_place_id_created_maps_to_using("Mike Rezek", "getPlaceAPI");
+	}
+	}
+}
